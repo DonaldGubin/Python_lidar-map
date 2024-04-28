e38bc99384f3e0b990eee30568d016098055e108
DonaldGubin <bassboosted111@mail.ru>
2024-04-29 00:11:16 +0300

    'update'

--- createlidarmap.py
+++ createlidarmap.py
@@ -14,12 +14,12 @@ with open("examp10.txt", 'r') as f:
         robot_angle.append(z)
         mas.append(stroka)
 lidar_measurements = np.array([row[3:] for row in mas], dtype=float)
-plt.figure(figsize=(15,10))
+
+plt.figure(figsize=(10, 10), facecolor='pink')
 for i in range(100):
     x = robot_x[i]
     y = robot_y[i]
     angle = robot_angle[i]
-    # Преобразование замеров лидара в координаты объектов с использованием numpy
     filtered_coordinates = []
     for j in range(681):
         lidar_distance = float(lidar_measurements[i][j])
@@ -27,15 +27,16 @@ for i in range(100):
         object_x = x + lidar_distance * np.cos(lidar_angle)
         object_y = y + lidar_distance * np.sin(lidar_angle)
         if 1 <= lidar_distance < 4:
+            object_x += np.random.normal(0, 0.1)
+            object_y += np.random.normal(0, 0.1)
             filtered_coordinates.append((object_x, object_y))
     object_coordinates = np.array(filtered_coordinates)
     # Отображение объектов, обнаруженных лидаром
-    plt.scatter(object_coordinates[:, 0], object_coordinates[:, 1], color='red')
+    plt.scatter(object_coordinates[:, 0], object_coordinates[:, 1], color='red', s=2)
+plt.plot(robot_x, robot_y, color='green' if i == 0 else None)
 
-plt.plot(robot_x, robot_y, color='blue', label='Маршрут робота' if i == 0 else None)
 plt.xlabel('X')
 plt.ylabel('Y')
 plt.title('Карта с маршрутом и объектами, обнаруженными лидаром')
-plt.legend()
 plt.grid(True)
 plt.show()
